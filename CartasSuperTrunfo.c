2169b0a7e4cc7d020421796cfe4bc46881112591
TiagoBlackZord <eternidade498@gmail.com>
2025-08-14 22:51:29 +0000

    Finalização do Super Trunfo Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,10 @@
 int main(){
 
 	char estado1, estado2, codigoCarta1[4], codigoCarta2[4], nomeCidade1[25], nomeCidade2[25];
-	int pontosTuristicos1, pontosTuristicos2;
-	float area1, area2, pib1, pib2, densidadePopulacional1, densidadePopulacional2, PibPerCapita1, PibPerCapita2;
+	int pontosTuristicos1, pontosTuristicos2, resultadoMaior, carta1, carta2, indice;
+	float area1, area2, pib1, pib2, densidadePopulacional1, densidadePopulacional2, PibPerCapita1, PibPerCapita2, SuperPoder1, SuperPoder2;
 	unsigned long int populacao1, populacao2;
+	const char* mensagens[3] = {"São iguais","Carta 1 venceu","Carta 2 venceu"}; // Array de mensagens
 
 
 	//Recebendo valores do usuário para a carta 1
@@ -41,12 +42,75 @@ int main(){
 	printf("\nInforme a quantidade de pontos turisticos do Estado: ");
 	scanf("%d", &pontosTuristicos2);
 
-    densidadePopulacional1 = (float) populacao1 / area1;
-    densidadePopulacional2 = (float) populacao2 / area2;
+	//Calculando a Densidade Populacional, PIB per capita e os Super Poderes
+    densidadePopulacional1 = populacao1 / area1;
+    densidadePopulacional2 = populacao2 / area2;
 
-    PibPerCapita1 = (float) pib1 / populacao1;
-    PibPerCapita2 = (float) pib2 / populacao2;
+    PibPerCapita1 = pib1 / populacao1;
+    PibPerCapita2 = pib2 / populacao2;
 
+	SuperPoder1 = (populacao1 + area1 + pib1 + pontosTuristicos1 + PibPerCapita1)-densidadePopulacional1;
+	SuperPoder2 = (populacao2 + area2 + pib2 + pontosTuristicos2 + PibPerCapita2)-densidadePopulacional2;
+
+
+	printf("\n\nComparação de Cartas:");
+	//Calculando quem é maior
+	carta1 = populacao1 > populacao2;
+	carta2 = populacao2 > populacao1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("\nPopulação: %s\n", mensagens[indice]);
+
+	//Calculando quem é maior
+	carta1 = area1 > area2;
+	carta2 = area2 > area1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Área: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = pib1 > pib2;
+	carta2 = pib2 > pib1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("PIB: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = pontosTuristicos1 > pontosTuristicos2;
+	carta2 = pontosTuristicos2 > pontosTuristicos1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Pontos Turisticos: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = densidadePopulacional1 < densidadePopulacional2;
+	carta2 = densidadePopulacional2 < densidadePopulacional1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Densidade Populacional: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = PibPerCapita1 > PibPerCapita2;
+	carta2 = PibPerCapita2 > PibPerCapita1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("PIB per capita: %s\n", mensagens[indice]);
+
+	//Calculando quem é maior
+	carta1 = SuperPoder1 > SuperPoder2;
+	carta2 = SuperPoder2 > SuperPoder1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Super Poder: %s\n\n", mensagens[indice]);
+	
+/*
 	//Monstrando a carta 1
 	printf("\n\nCarta 1:");
 	printf("\nEstado: %c", estado1);
@@ -73,7 +137,7 @@ int main(){
     printf("\nDensidade Populacional: %.2f", densidadePopulacional2);
     printf("\nPIB Per Capita: %.2f", PibPerCapita2);
     printf("\n\n");
-
+*/
     return 0;
 
 }
