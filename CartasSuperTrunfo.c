4c9c9e6a79816629583a5cf1a68a0d0ce4ece0ec
TiagoBlackZord <eternidade498@gmail.com>
2025-08-14 01:06:31 +0000

    Unsigned long int Populacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,8 +2,9 @@
 int main(){
 
 	char estado1, estado2, codigoCarta1[4], codigoCarta2[4], nomeCidade1[25], nomeCidade2[25];
-	int populacao1, populacao2, pontosTuristicos1, pontosTuristicos2;
+	int pontosTuristicos1, pontosTuristicos2;
 	float area1, area2, pib1, pib2, densidadePopulacional1, densidadePopulacional2, PibPerCapita1, PibPerCapita2;
+	unsigned long int populacao1, populacao2;
 
 
 	//Recebendo valores do usuário para a carta 1
@@ -15,7 +16,7 @@ int main(){
 	printf("\nInforme o nome cidade: ");
 	scanf(" %[^\n]", nomeCidade1);
 	printf("\nInforme quantidade total da população no Estado: ");
-	scanf("%d", &populacao1);
+	scanf("%lu", &populacao1);
 	printf("\nInforme a área total do Estado: ");
 	scanf("%f", &area1);
 	printf("\nInforme o PIB do Estado: ");
@@ -32,7 +33,7 @@ int main(){
 	printf("\nInforme o nome cidade: ");
 	scanf(" %[^\n]", nomeCidade2);
 	printf("\nInforme quantidade total da população no Estado: ");
-	scanf("%d", &populacao2);
+	scanf("%lu", &populacao2);
 	printf("\nInforme a área total do Estado: ");
 	scanf("%f", &area2);
 	printf("\nInforme o PIB do Estado: ");
@@ -51,7 +52,7 @@ int main(){
 	printf("\nEstado: %c", estado1);
 	printf("\nCódigo: %s", codigoCarta1);
 	printf("\nNome da Cidade: %s", nomeCidade1);
-	printf("\nPopulação: %d", populacao1);
+	printf("\nPopulação: %lu", populacao1);
 	printf("\nÁrea: %.2f", area1);
 	printf("\nPIB: %.2f", pib1);
 	printf("\nNúmeros de Pontos Turísticos: %d",pontosTuristicos1);
@@ -65,7 +66,7 @@ int main(){
 	printf("\nEstado: %c", estado2);
 	printf("\nCódigo: %s", codigoCarta2);
 	printf("\nNome da Cidade: %s", nomeCidade2);
-	printf("\nPopulação: %d", populacao2);
+	printf("\nPopulação: %lu", populacao2);
 	printf("\nÁrea: %.2f", area2);
 	printf("\nPIB: %.2f", pib2);
 	printf("\nNúmeros de Pontos Turísticos: %d",pontosTuristicos2);
