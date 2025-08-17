7115bc21da0bafa7348d6c272fc2c38343d27294
TiagoBlackZord <eternidade498@gmail.com>
2025-08-17 01:21:27 -0300

    Update CartasSuperTrunfo.c
    Voltando ao estado anterior para enviar o código que eu esqueci de mandar.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-#include <string.h>
 int main(){
 
-	char estado1, estado2, codigoCarta1[4], codigoCarta2[4], nomeCidade1[25], nomeCidade2[25], escolhaDoUsuarioEscrita[30];
-	int pontosTuristicos1, pontosTuristicos2, populacao1, populacao2, escolhaDoUsuario;
-	float area1, area2, pib1, pib2, densidadePopulacional1, densidadePopulacional2, PibPerCapita1, PibPerCapita2;
+	char estado1, estado2, codigoCarta1[4], codigoCarta2[4], nomeCidade1[25], nomeCidade2[25];
+	int pontosTuristicos1, pontosTuristicos2, resultadoMaior, carta1, carta2, indice;
+	float area1, area2, pib1, pib2, densidadePopulacional1, densidadePopulacional2, PibPerCapita1, PibPerCapita2, SuperPoder1, SuperPoder2;
+	unsigned long int populacao1, populacao2;
+	const char* mensagens[3] = {"São iguais","Carta 1 venceu","Carta 2 venceu"}; // Array de mensagens
+
 
 	//Recebendo valores do usuário para a carta 1
-	printf("\n\n*** Carta 1 ***\n");
+	printf("\n\nCarta 1\n");
 	printf("\nInforme a primeira letra do Estado: ");
 	scanf("%c", &estado1);
 	printf("\nInforme o código da carta. Ex: 01, 02, 03 ou 04: ");
@@ -15,7 +17,7 @@ int main(){
 	printf("\nInforme o nome cidade: ");
 	scanf(" %[^\n]", nomeCidade1);
 	printf("\nInforme quantidade total da população no Estado: ");
-	scanf("%d", &populacao1);
+	scanf("%lu", &populacao1);
 	printf("\nInforme a área total do Estado: ");
 	scanf("%f", &area1);
 	printf("\nInforme o PIB do Estado: ");
@@ -32,7 +34,7 @@ int main(){
 	printf("\nInforme o nome cidade: ");
 	scanf(" %[^\n]", nomeCidade2);
 	printf("\nInforme quantidade total da população no Estado: ");
-	scanf("%d", &populacao2);
+	scanf("%lu", &populacao2);
 	printf("\nInforme a área total do Estado: ");
 	scanf("%f", &area2);
 	printf("\nInforme o PIB do Estado: ");
@@ -40,123 +42,75 @@ int main(){
 	printf("\nInforme a quantidade de pontos turisticos do Estado: ");
 	scanf("%d", &pontosTuristicos2);
 
-	//Calculando a Densidade Populacional e o PIB per capita
+	//Calculando a Densidade Populacional, PIB per capita e os Super Poderes
     densidadePopulacional1 = populacao1 / area1;
     densidadePopulacional2 = populacao2 / area2;
 
     PibPerCapita1 = pib1 / populacao1;
     PibPerCapita2 = pib2 / populacao2;
 
-	//Criando menu interativo
-	printf("\n\nAgora, você deve escolher uum atributo para comparar as cartas!\nVocê pode escolher entre:\n\n");
-	printf("1. População\n2. Área\n3. PIB\n4. Número de Pontos Turísticos\n5. Densidade Demográfica\n\n Digite um número de opção: ");
-	scanf("%d", &escolhaDoUsuario);
+	SuperPoder1 = (populacao1 + area1 + pib1 + pontosTuristicos1 + PibPerCapita1)-densidadePopulacional1;
+	SuperPoder2 = (populacao2 + area2 + pib2 + pontosTuristicos2 + PibPerCapita2)-densidadePopulacional2;
 
-	//Pegando a escolha escrita do usuário
-	if(escolhaDoUsuario == 1){
-		strcpy(escolhaDoUsuarioEscrita, "População");
-	} else if(escolhaDoUsuario == 2){
-		strcpy(escolhaDoUsuarioEscrita, "Área");
-	} else if(escolhaDoUsuario == 3){
-		strcpy(escolhaDoUsuarioEscrita, "PIB");
-	} else if(escolhaDoUsuario == 4){
-		strcpy(escolhaDoUsuarioEscrita, "Número de Pontos Turísticos");
-	} else if(escolhaDoUsuario == 5){
-		strcpy(escolhaDoUsuarioEscrita, "Densidade Demográfica");
-	}
+
+	printf("\n\nComparação de Cartas:");
 	
-	//
-	if(escolhaDoUsuario > 5 || escolhaDoUsuario < 1){
-		printf("\n\nVocê escolheu uma opção que não existe!\nExecute o programa novamente.\n\n");
-	}else{
-		switch (escolhaDoUsuario)
-		{
-		case 1:
-			if(populacao1 > populacao2){
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %d     Carta 2: %d\n", populacao1, populacao2);
-				printf("*** Carta 1 venceu! ***\n\n");
-			}else{
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %d     Carta 2: %d\n", populacao1, populacao2);
-				printf("*** Carta 2 venceu! ***\n\n");
-			}
-			break;
-		case 2:
-			if(area1 > area2){
-				printf("\n\nResultado:\n");
-				printf("%s      %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %f     Carta 2: %f\n", area1, area2);
-				printf("*** Carta 1 venceu! ***\n\n");
-			}else{
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %f     Carta 2: %f\n", area1, area2);
-				printf("*** Carta 2 venceu! ***\n\n");
-			}
-			break;
-		case 3:
-			if(pib1 > pib2){
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %f     Carta 2: %f\n", pib1, pib2);
-				printf("*** Carta 1 venceu! ***\n\n");
-			}else{
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %f     Carta 2: %f\n", pib1, pib2);
-				printf("*** Carta 2 venceu! ***\n\n");
-			}
-			break;
-		case 4:
-			if(pontosTuristicos1 > pontosTuristicos2){
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %d     Carta 2: %d\n", pontosTuristicos1, pontosTuristicos2);
-				printf("*** Carta 1 venceu! ***\n\n");
-			}else{
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %d     Carta 2: %d\n", pontosTuristicos1, pontosTuristicos2);
-				printf("*** Carta 2 venceu! ***\n\n");
-			}
-			break;
-		case 5:
-			if(densidadePopulacional1 < densidadePopulacional2){
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %f     Carta 2: %f\n", densidadePopulacional1, densidadePopulacional2);
-				printf("*** Carta 1 venceu! ***\n\n");
-			}else{
-				printf("\n\nResultado:\n");
-				printf("%s     %s\n", nomeCidade1, nomeCidade2);
-				printf("Atributo escolhido: %s\n", escolhaDoUsuarioEscrita);
-				printf("Carta 1: %f     Carta 2: %f\n", densidadePopulacional1, densidadePopulacional2);
-				printf("*** Carta 2 venceu! ***\n\n");
-			}
-			break;
-
-		
-		default:
-			printf("Empate!");
-			break;
-		}
-	}
-
+	//Calculando quem é maior
+	carta1 = populacao1 > populacao2;
+	carta2 = populacao2 > populacao1;
 	
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("\nPopulação: %s\n", mensagens[indice]);
 
+	//Calculando quem é maior
+	carta1 = area1 > area2;
+	carta2 = area2 > area1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Área: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = pib1 > pib2;
+	carta2 = pib2 > pib1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("PIB: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = pontosTuristicos1 > pontosTuristicos2;
+	carta2 = pontosTuristicos2 > pontosTuristicos1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Pontos Turisticos: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = densidadePopulacional1 < densidadePopulacional2;
+	carta2 = densidadePopulacional2 < densidadePopulacional1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Densidade Populacional: %s\n", mensagens[indice]);
+	
+	//Calculando quem é maior
+	carta1 = PibPerCapita1 > PibPerCapita2;
+	carta2 = PibPerCapita2 > PibPerCapita1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("PIB per capita: %s\n", mensagens[indice]);
 
+	//Calculando quem é maior
+	carta1 = SuperPoder1 > SuperPoder2;
+	carta2 = SuperPoder2 > SuperPoder1;
+    // Índice da mensagem: 0 se iguais, 1 se carta 1 for maior, 2 se carta 2 for maior
+    indice = carta1 * 1 + carta2 * 2;
+	//Mostrando Resultado
+    printf("Super Poder: %s\n\n", mensagens[indice]);
     return 0;
 
 }
